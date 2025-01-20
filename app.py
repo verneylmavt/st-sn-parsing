3bb04b4aba52f063b33aa53cec9153e374ed36e1
Elvern Neylmav Tanny <elvernneylmav@gmail.com>
2025-01-20 23:32:53 +0800

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def main():
-    st.title("Syntactic Parsing")
+    st.title("Syntactic Parsing 🌲")
     st.write("""
-        ## Coming Soon
+        #### Work in Progress...
         Elvern Neylmav T is currently working on this page. Please check back later for updates. :smile:
     """)
     # st.image("https://via.placeholder.com/400x200.png?text=Coming+Soon", use_column_width=True)
